62666da7c8fdf57abd44013ef10540c0f581225a
Adam Vaden <avaden@prattmiller.com>
2015-12-18 08:48:15 -0500

    Started factory

---
+++ DesignPatterns/Factory/PizzaStore.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factory
+{
+    public abstract class PizzaStore
+    {
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza;
+
+            pizza = CreatePizza(type);
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+
+        public abstract Pizza CreatePizza(string type);
+    }
+
+    public class NYPizzaStore : PizzaStore
+    {
+        public override Pizza CreatePizza(string type)
+        {
+            if (type == "cheese")
+            {
+                return new NYStyleCheesePizza();
+            }
+            else if (type == "veggie")
+            {
+                return new NYStyleVeggiePizza();
+            }
+            else if (type == "clam")
+            {
+                return new NYStyleClamPizza();
+            }
+            else if (type == "pepperoni")
+            {
+                return new NYStylePepperoniPizza();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class ChicagoPizzaStore : PizzaStore
+    {
+        public override Pizza CreatePizza(string type)
+        {
+            if (type == "cheese")
+            {
+                return new ChicagoStyleCheesePizza();
+            }
+            else if (type == "veggie")
+            {
+                return new ChicagoStyleVeggiePizza();
+            }
+            else if (type == "clam")
+            {
+                return new ChicagoStyleClamPizza();
+            }
+            else if (type == "pepperoni")
+            {
+                return new ChicagoStylePepperoniPizza();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class CaliforniaPizzaStore : PizzaStore
+    {
+        public override Pizza CreatePizza(string type)
+        {
+            if (type == "cheese")
+            {
+                return new CaliforniaStyleCheesePizza();
+            }
+            else if (type == "veggie")
+            {
+                return new CaliforniaStyleVeggiePizza();
+            }
+            else if (type == "clam")
+            {
+                return new CaliforniaStyleClamPizza();
+            }
+            else if (type == "pepperoni")
+            {
+                return new CaliforniaStylePepperoniPizza();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Factory/PizzaTypes.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factory
+{
+    public abstract class Pizza
+    {
+        internal string type;
+
+        public Pizza()
+        { }
+
+        public void Prepare()
+        { }
+
+        public void Bake()
+        { }
+
+        public void Cut()
+        { }
+
+        public void Box()
+        { }
+    }
+
+    public class NYStyleCheesePizza : Pizza
+    {
+        public NYStyleCheesePizza()
+        {
+            this.type = "NYStyleCheesePizza";
+        }
+    }
+
+    public class NYStyleClamPizza : Pizza
+    {
+        public NYStyleClamPizza()
+        {
+            this.type = "NYStyleClamPizza";
+        }
+    }
+
+    public class NYStylePepperoniPizza : Pizza
+    {
+        public NYStylePepperoniPizza()
+        {
+            this.type = "NYStylePepperoniPizza";
+        }
+    }
+
+    public class NYStyleVeggiePizza : Pizza
+    {
+        public NYStyleVeggiePizza()
+        {
+            this.type = "NYStyleVeggiePizza";
+        }
+    }
+
+    public class ChicagoStyleCheesePizza : Pizza
+    {
+        public ChicagoStyleCheesePizza()
+        {
+            this.type = "ChicagoStyleCheesePizza";
+        }
+    }
+
+    public class ChicagoStyleClamPizza : Pizza
+    {
+        public ChicagoStyleClamPizza()
+        {
+            this.type = "ChicagoStyleClamPizza";
+        }
+    }
+
+    public class ChicagoStylePepperoniPizza : Pizza
+    {
+        public ChicagoStylePepperoniPizza()
+        {
+            this.type = "ChicagoStylePepperoniPizza";
+        }
+    }
+
+    public class ChicagoStyleVeggiePizza : Pizza
+    {
+        public ChicagoStyleVeggiePizza()
+        {
+            this.type = "ChicagoStyleVeggiePizza";
+        }
+    }
+
+    public class CaliforniaStyleCheesePizza : Pizza
+    {
+        public CaliforniaStyleCheesePizza()
+        {
+            this.type = "CaliforniaStyleCheesePizza";
+        }
+    }
+
+    public class CaliforniaStyleClamPizza : Pizza
+    {
+        public CaliforniaStyleClamPizza()
+        {
+            this.type = "CalforniaStyleClamPizza";
+        }
+    }
+
+    public class CaliforniaStylePepperoniPizza : Pizza
+    {
+        public CaliforniaStylePepperoniPizza()
+        {
+            this.type = "CalforniaStylePepperoniPizza";
+        }
+    }
+
+    public class CaliforniaStyleVeggiePizza : Pizza
+    {
+        public CaliforniaStyleVeggiePizza()
+        {
+            this.type = "CalforniaStyleVeggiePizza";
+        }
+    }
+}
