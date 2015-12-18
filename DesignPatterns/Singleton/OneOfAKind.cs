356e812599db593daa397ce0f4eda5da7fc08b55
A. Vaden <abvaden801@gmail.com>
2015-12-18 17:46:42 -0500

    Added Singleton example with several differnt types of making it thread safe and test to accompany

---
+++ DesignPatterns/Singleton/OneOfAKind.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class NotThreadSafe
+    {
+        private static NotThreadSafe OneOfAKindInstance;
+
+        public string ID
+        {
+            get
+            {
+                return m_ID.ToString();
+            }
+        }
+
+        private Guid m_ID;
+
+        private NotThreadSafe()
+        {
+            m_ID = Guid.NewGuid();
+        }
+
+        public static NotThreadSafe GetInstance()
+        {
+            if (OneOfAKindInstance == null)
+            {
+                OneOfAKindInstance = new NotThreadSafe();
+            }
+            return OneOfAKindInstance;
+        }
+    }
+
+    public class VolatileThreadSafe
+    {
+        private volatile static VolatileThreadSafe OneOfAKindInstance;
+
+        public string ID
+        {
+            get
+            {
+                return m_ID.ToString();
+            }
+        }
+
+        private Guid m_ID;
+
+        private VolatileThreadSafe()
+        {
+            m_ID = Guid.NewGuid();
+        }
+
+        public static VolatileThreadSafe GetInstance()
+        {
+            if (OneOfAKindInstance == null)
+            {
+                OneOfAKindInstance = new VolatileThreadSafe();
+            }
+            return OneOfAKindInstance;
+        }
+    }
+
+    public class LockThreadSafe
+    {
+        private static LockThreadSafe OneOfAKindInstance;
+        private static object instaceLock = new object();
+
+        public string ID
+        {
+            get
+            {
+                return m_ID.ToString();
+            }
+        }
+
+        private Guid m_ID;
+
+        private LockThreadSafe()
+        {
+            m_ID = Guid.NewGuid();
+        }
+
+        public static LockThreadSafe GetInstance()
+        {
+            lock(instaceLock)
+            {
+                if (OneOfAKindInstance == null)
+                {
+                    OneOfAKindInstance = new LockThreadSafe();
+                }
+            }
+            
+            return OneOfAKindInstance;
+        }
+    }
+}
---
+++ DesignPatternsTests/SingletonTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DesignPatterns.Singleton;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTests
+{
+    [TestClass]
+    public class SingletonTest
+    {
+        [TestMethod]
+        [TestCategory("Singleton")]
+        public void NoThreadProtection()
+        {
+            NotThreadSafe a = NotThreadSafe.GetInstance();
+            NotThreadSafe b = NotThreadSafe.GetInstance();
+
+            Assert.AreEqual(a.ID, b.ID);
+        }
+
+        [TestMethod]
+        [TestCategory("Singleton")]
+        public void VolitleThreadSage()
+        {
+            VolatileThreadSafe a = VolatileThreadSafe.GetInstance();
+            VolatileThreadSafe b = VolatileThreadSafe.GetInstance();
+
+            Assert.AreEqual(a.ID, b.ID);
+        }
+
+        [TestMethod]
+        [TestCategory("Singleton")]
+        public void LockThreadSage()
+        {
+            LockThreadSafe a = LockThreadSafe.GetInstance();
+            LockThreadSafe b = LockThreadSafe.GetInstance();
+
+            Assert.AreEqual(a.ID, b.ID);
+        }
+    }
+}
